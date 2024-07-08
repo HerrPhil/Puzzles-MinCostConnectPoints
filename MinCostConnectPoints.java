23cf4751bed04828f3b8fd88d72be668f0dc9aee
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-07-08 17:52:18 -0600

    Complete Prim's Algorithm

--- MinCostConnectPoints.java
+++ MinCostConnectPoints.java
@@ -47,30 +47,59 @@ public class MinCostConnectPoints {
     }
 
     public int findMinimumCost(List<List<Integer>> points) {
+
         System.out.printf("Find the minimum cost of connected points in a minimum span tree%n");
 
-        PriorityQueue<Integer> intQueue = new PriorityQueue<>();
+        // The following will be Prim's algorithm to create a MST (Minimum Spanning Tree)
+        int n = points.size();
+        int totalCost = 0;
+        PriorityQueue<MinimumTreeSpan> heap = new PriorityQueue<>(n, new SortByDistance());
+        // This special minimum tree span is the distance of the first point from itself.
+        heap.add(new MinimumTreeSpan(0, 0));
+        Set<Integer> seen = new TreeSet<>();
 
-        intQueue.add(1);
-        intQueue.add(5);
-        intQueue.add(2);
-        intQueue.add(4);
-        intQueue.add(3);
+        while (seen.size() < n) {
 
-        while(intQueue.peek() != null) {
-            int value = intQueue.poll();
-            System.out.printf("from the top of the heap %d%n", value);
+            System.out.printf("%n%nwhile the size of seen points is less than n%n");
+            int distance = 0;
+            int pointIndex = 0;
+            MinimumTreeSpan nextMinimumTreeSpan = heap.poll();
+            if (nextMinimumTreeSpan != null) {
+                distance = nextMinimumTreeSpan.getDistance();
+                pointIndex = nextMinimumTreeSpan.getPointIndex();
+            }
+            System.out.printf("The following is the next minimum tree span%n%s", nextMinimumTreeSpan.toString());
+            if (seen.contains(pointIndex)) continue;
+            System.out.printf("The seen set does not contain point index = %d%n", pointIndex);
+            seen.add(pointIndex);
+            System.out.printf("Added pointIndex = %d to seen set%n", pointIndex);
+            totalCost += distance;
+            System.out.printf("Added distance = %d to total cost%n", distance);
+            int xOfPointIndex = points.get(pointIndex).get(0);
+            int yOfPointIndex = points.get(pointIndex).get(1);
+
+            for (int j = 0; j < n; j++) {
+
+                System.out.printf("For jth point index = %d%n", j);
+                if (!seen.contains(j)) {
+
+                    int xOfPointJ = points.get(j).get(0);
+                    int yOfPointJ = points.get(j).get(1);
+                    // Since we are not concerned with exact geometry,
+                    // it is not necessary to square the differences and square root the sum,
+                    // to get the hypotenuse (Pythagorean theorem).
+                    // The sum of differences of coordinates is good enough to estimate distance.
+                    int nextDistance = Math.abs(xOfPointIndex - xOfPointJ) + Math.abs(yOfPointIndex - yOfPointJ);
+                    heap.offer(new MinimumTreeSpan(nextDistance, j));
+                    System.out.printf("pushed to heap minimum tree span of distance = %d and point index = %d%n", nextDistance, j);
+                }
+                System.out.printf("goto to next j%n");
+
+            }
+
         }
 
-        MinimumTreeSpan span = new MinimumTreeSpan(123, 1);
-        System.out.printf("%s%n", span.toString());
-        System.out.printf("hash code is %d%n", span.hashCode());
-        System.out.printf("equals to span expect true actual = %b%n", span.equals(new MinimumTreeSpan(123,1)));
-        System.out.printf("equals to span expect false actual = %b%n", span.equals(new MinimumTreeSpan(123,0)));
-        System.out.printf("equals to string expect false actual = %b%n", span.equals("test"));
-        System.out.printf("equals to null expect false actual = %b%n", span.equals(null));
-
-        return -1;
+        return totalCost;
     }
 
 }
